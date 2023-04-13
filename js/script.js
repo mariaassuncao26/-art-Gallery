5cf53f5bb2ce3d10d92e72f2b9f2458e61e90834
2º Téc. Desenvolvimento de Sistemas <ds-tec-2@servidor.acad>
2023-04-13 14:44:50 -0300

    Atualizado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var nquadro = ["A casa amarela", "Amendoeira em flor", "A noite estrelada", "Autorretrato com chapéu de palha"];
+/*var nquadro = ["A casa amarela", "Amendoeira em flor", "A noite estrelada", "Autorretrato com chapéu de palha"];
 
 function quadro1(){
     var nome = document.getElementById("nome1");
@@ -17,6 +17,35 @@ function quadro4(){
     nome.innerHTML = nquadro[3];
 }
 
-function tquadros(){
-    
+
+
+HTML
+    <script src="js/script.js"></script>
+    <div>
+        <img src="imgs/acasaamarela.jpg" class="quadros" onclick="quadro1()" alt="img">
+        <h1 id="nome1" style="color: black; font-size: 20px;"></h1>
+    </div>
+    <div>
+        <img src="imgs/amendoeiraemflor.jpg" class="quadros" onclick="quadro2()" alt="img">
+        <h1 id="nome2" style="color: black; font-size: 20px;"></h1>
+    </div>
+    <div>
+        <img src="imgs/anoiteestrelada.jpg" class="quadros" onclick="quadro3()" alt="img">
+        <h1 id="nome3" style="color: black; font-size: 20px;"></h1>
+    </div>
+    <div>
+        <img src="imgs/autorretratocomchapeudepalha.jpg" class="quadros" onclick="quadro4()" alt="img">
+        <h1 id="nome4" style="color: black; font-size: 20px;"></h1>
+    </div>
+    <div>
+
+    </div>
+
+*/
+
+var nquadro = ["A casa amarela", "Amendoeira em flor", "A noite estrelada", "Autorretrato com chapéu de palha"];
+
+function pintor1(){
+    var nome = document.getElementById("nome1");
+    nome.innerHTML = nquadro[0];
 }
