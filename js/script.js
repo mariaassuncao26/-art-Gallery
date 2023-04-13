70492dafd2184759f2b2306f4826368b55a1cf2f
2º Téc. Desenvolvimento de Sistemas <ds-tec-2@servidor.acad>
2023-04-13 12:59:38 -0300

    Atualizado

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var nquadro = ["A casa amarela", "Amendoeira em flor", "A noite estrelada", "Autorretrato com chapéu de palha"];
+
+function quadro1(){
+    var nome = document.getElementById("nome1");
+    nome.innerHTML = nquadro[0];
+}
+function quadro2(){
+    var nome = document.getElementById("nome2");
+    nome.innerHTML = nquadro[1];
+}
+function quadro3(){
+    var nome = document.getElementById("nome3");
+    nome.innerHTML = nquadro[2];
+}
+function quadro4(){
+    var nome = document.getElementById("nome4");
+    nome.innerHTML = nquadro[3];
+}
+
+function tquadros(){
+    
+}
